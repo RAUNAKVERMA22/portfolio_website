b42617409c8c00b0bf024dbd033dc67ff3c78085
RAUNAKVERMA22 <raunakverma022@gmail.com>
2025-04-20 19:41:28 +0530

    updated

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -24,31 +24,31 @@ function Home2() {
               <br />
               <br />I am fluent in classics like
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> C++, Java and Python. </b>
               </i>
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
+                <b className="purple">Projects and Products </b> and
                 also in areas related to{" "}
                 <b className="purple">
-                  Blockchain.
+                  Data Science.
                 </b>
               </i>
               <br />
               <br />
               Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+              with <b className="purple">Python</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  Machine Learning Algorithms and Different Python Libraries
                 </b>
               </i>
               &nbsp; like
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> Numpy , Pandas and Pytorch </b>
               </i>
             </p>
           </Col>
